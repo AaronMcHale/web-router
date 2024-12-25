40a6efc1d06f8796740ecddf6648fea8c06a3789
Aaron McHale <aaron@mchale.me>
2024-12-25 00:18:51 +0000

    Add more tests and dynamic loading of service docker-compose.yml

--- env.sh
+++ env.sh
@@ -14,10 +14,14 @@ export PGID="$(id -g)"
 
 # Set defaults for services
 # -------------------------
+export COMPOSE_FILE="docker-compose.yml"
 for service in ./services/*; do
+  if [ "$service" = "./services/*" ]; then
+    # If $service = ./services/* it means it couldn't find any services
+    break
+  fi
   if [ -f "$service""/defaults.env.sh" ]; then
     . "$service""/defaults.env.sh"
-    echo "$service""/defaults.env.sh"
   fi
 done
 
@@ -30,3 +34,30 @@ else
   # Warn if .env file doesn't exist
   echo "WARNING: .env does not exist."
 fi
+
+# Load services
+# -------------
+for service in ./services/*; do
+  if [ "$service" = "./services/*" ]; then
+    # If $service = ./services/* it means it couldn't find any services
+    break
+  fi
+  service_name="${service##*/}"
+  service_is_enabled_var_name="SERVICE_ENABLED_""${service_name^^}"
+  service_is_enabled=0
+  # Check if the `SERVICE_ENABLED_` variable is set for this service.
+  # Pipe the output of `env` to `grep` and check for lines starting with
+  # the variable name, then assign its value to `service_is_enabled`.
+  if [ "$(env | grep ^""$service_is_enabled_var_name"" )" ]; then
+    service_is_enabled="${!service_is_enabled_var_name}"
+  fi
+  if [ "$service_is_enabled" = 1 ]; then
+    if [ -f "$service""/docker-compose.yml" ]; then
+      export COMPOSE_FILE="$COMPOSE_FILE"":""$service""/docker-compose.yml"
+    fi
+    if [ -f "$service""/env.sh" ]; then
+      . "$service""/env.sh"
+    fi
+  fi
+  unset service_name service_is_enabled_var_name service_is_enabled
+done
--- tests/test-dynamic-service-loading.sh
+++ tests/test-dynamic-service-loading.sh
@@ -6,12 +6,15 @@ clean_exit() {
 }
 trap clean_exit EXIT
 
+# Setup test service
+# ------------------
+
 echo "Creating service for running tests with random name..."
 rand="$RANDOM"
 if [ -z "$rand" ]; then
   echo "Error: unable to generate random number: output of \$RANDOM is empty."; exit 1
 fi
-test_service_name="test-""$rand"
+test_service_name="test_""$rand"
 test_service_dir="../services/""$test_service_name"
 if [ -x "$test_service_dir" ]; then
   echo "Error: test service already exists at ""$test_service_dir"; exit 1
@@ -20,35 +23,264 @@ echo "mkdir ""$test_service_dir"
 mkdir -p "$test_service_dir"
 echo -e "OK\n"
 
+# Setting up env.sh for tests
+# ---------------------------
+
 echo "Creating defaults.env.sh for test service..."
 defaults_env_sh_path="$test_service_dir""/defaults.env.sh"
-echo "export DEFAULT_VALUE_TEST=""$test_service_name" > "$defaults_env_sh_path"
-echo "Wrote to ""$defaults_env_sh_path"
+echo "export DEFAULT_ENV_SH_TEST=""$test_service_name" > "$defaults_env_sh_path"
+if [ ! -e "$defaults_env_sh_path" ]; then
+  echo "Test script error: failed to write to ""$defaults_env_sh_path"" file does not exist."; exit 1
+fi
+echo "Wrote to ""$defaults_env_sh_path:"
+cat "$defaults_env_sh_path"
 echo -e "OK\n"
 
-echo "Removing DEFAULT_VALUE_TEST variable from .env if it was previously set..."
+echo "Removing DEFAULT_ENV_SH_TEST variable from .env if it was previously set..."
 if [ -f ../.env ]; then
-  sed -i '/^DEFAULT_VALUE_TEST/d' ../.env
+  sed -i '/^DEFAULT_ENV_SH_TEST/d' ../.env
+  if [ "$(grep ^DEFAULT_ENV_SH_TEST ../.env)" ]; then
+    echo "Test script error: DEFAULT_ENV_SH_TEST variable still set in .env after removing."; exit 1
+  fi
 fi
 echo -e "OK\n"
+
+# Testing defaults.env.sh
+# -----------------------
 
 echo "Testing default.env.sh..."
 cd ..
 . env.sh
 cd tests
-if [ "$DEFAULT_VALUE_TEST" != "$test_service_name" ]; then
-  echo "Error: DEFAULT_VALUE_TEST env variable is not expected value: expecting: ""$test_service_name""; got: ""$DEFAULT_VALUE_TEST"; exit 1
+if [ -z "${DEFAULT_ENV_SH_TEST-}" ] ; then
+  echo "Error: DEFAULT_ENV_SH_TEST env variable is empty."; exit 1
 fi
-echo "DEFAULT_VALUE_TEST" is "$test_service_name"
+if [ "$DEFAULT_ENV_SH_TEST" != "$test_service_name" ]; then
+  echo "Error: DEFAULT_ENV_SH_TEST env variable is not expected value: expecting: ""$test_service_name""; got: ""$DEFAULT_ENV_SH_TEST"; exit 1
+fi
+echo "DEFAULT_ENV_SH_TEST is ""$test_service_name"
 echo -e "OK\n"
 
-echo "Testing .env to override DEFAULT_VALUE_TEST..."
-echo "DEFAULT_VALUE_TEST=value_changed" >> ../.env
+echo "Testing .env to override DEFAULT_ENV_SH_TEST..."
+echo -e "\nDEFAULT_ENV_SH_TEST=value_changed" >> ../.env
 cd ..
 . env.sh
 cd tests
-if [ "$DEFAULT_VALUE_TEST" != "value_changed" ]; then
-  echo "Error: DEFAULT_VALUE_TEST env variable is not expected value: expecting: value_changed; got: ""$DEFAULT_VALUE_TEST"; exit 1
+if [ "$DEFAULT_ENV_SH_TEST" != "value_changed" ]; then
+  echo "Error: DEFAULT_ENV_SH_TEST env variable is not expected value: expecting: value_changed; got: ""$DEFAULT_ENV_SH_TEST"; exit 1
 fi
-echo "DEFAULT_VALUE_TEST is now: value_changed"
+echo "DEFAULT_ENV_SH_TEST is now: value_changed"
+echo -e "OK\n"
+
+echo "Removing DEFAULT_ENV_SH_TEST variable from .env..."
+sed -i '/^DEFAULT_ENV_SH_TEST/d' ../.env
+if [ "$(grep ^DEFAULT_ENV_SH_TEST ../.env)" ]; then
+  echo "Test script error: DEFAULT_ENV_SH_TEST variable still set in .env after removing."; exit 1
+fi
+echo -e "OK\n"
+
+unset DEFAULT_ENV_SH_TEST
+if [ -n "${DEFAULT_ENV_SH_TEST-}" ]; then
+  echo "Test script error: DEFAULT_ENV_SH_TEST variable still set after unsetting."; exit 1
+fi
+
+# Setting up env.sh for tests
+# ---------------------------
+
+echo "Creating env.sh for test service..."
+env_sh_path="$test_service_dir""/env.sh"
+echo "export ENV_SH_TEST=""$test_service_name" > "$env_sh_path"
+if [ ! -e "$env_sh_path" ]; then
+  echo "Test script error: failed to write to ""$env_sh_path"" file does not exist."; exit 1
+fi
+echo "Wrote to ""$env_sh_path:"
+cat "$env_sh_path"
+echo -e "OK\n"
+
+echo "Removing ENV_SH_TEST variable from .env if it was previously set..."
+if [ -f ../.env ]; then
+  sed -i '/^ENV_SH_TEST/d' ../.env
+  if [ "$(grep ^ENV_SH_TEST ../.env)" ]; then
+    echo "Test script error: ENV_SH_TEST variable still set in .env after removing."; exit 1
+  fi
+fi
+echo -e "OK\n"
+
+# SERVICE_ENABLED variable
+# ------------------------
+
+test_service_enable_var_name="SERVICE_ENABLED_${test_service_name^^}"
+
+echo "Removing ""$test_service_enable_var_name"" variable from .env if it was previously set..."
+if [ -f ../.env ]; then
+  sed -i "/^""$test_service_enable_var_name""/d" ../.env
+  if [ "$(grep ^$test_service_enable_var_name ../.env)" ]; then
+    echo "Test script error: ""$test_service_enable_var_name"" variable still set in .env after removing."; exit 1
+  fi
+fi
+echo -e "OK\n"
+
+# Testing enabling service
+# ------------------------
+
+echo "Writing to defaults.env.sh to enable test service by default..."
+echo "export ""$test_service_enable_var_name""=1" > "$defaults_env_sh_path"
+if [ ! -e "$defaults_env_sh_path" ]; then
+  echo "Test script error: failed to write to ""$defaults_env_sh_path"" file does not exist."; exit 1
+fi
+echo "Wrote to ""$defaults_env_sh_path:"
+cat "$defaults_env_sh_path"
+echo -e "OK\n"
+
+echo "Testing whether service gets enabled by checking for variable set in service's env.sh after sourcing root env.sh..."
+cd ..
+. env.sh
+cd tests
+if [ -z "${ENV_SH_TEST-}" ] ; then
+  echo "Error: ENV_SH_TEST env variable is empty."; exit 1
+fi
+if [ "$ENV_SH_TEST" != "$test_service_name" ]; then
+  echo "Error: ENV_SH_TEST env variable is not expected value: expecting: ""$test_service_name""; got: ""$ENV_SH_TEST"; exit 1
+fi
+echo -e "OK\n"
+
+unset ENV_SH_TEST
+if [ -n "${ENV_SH_TEST-}" ]; then
+  echo "Test script error: ENV_SH_TEST variable still set after unsetting."; exit 1
+fi
+
+# Testing disabling service
+# -------------------------
+
+echo "Writing to defaults.env.sh to disable test service by default..."
+echo "export ""$test_service_enable_var_name""=0" > "$defaults_env_sh_path"
+if [ ! -e "$defaults_env_sh_path" ]; then
+  echo "Test script error: failed to write to ""$defaults_env_sh_path"" file does not exist."; exit 1
+fi
+echo "Wrote to ""$defaults_env_sh_path:"
+cat "$defaults_env_sh_path"
+echo -e "OK\n"
+
+echo "Testing service now being disabled..."
+cd ..
+. env.sh
+cd tests
+if [ -n "${ENV_SH_TEST-}" ] ; then
+  echo "Error: ENV_SH_TEST env variable is set despite service being disabled."; exit 1
+fi
+echo -e "OK\n"
+
+# Testing re-enabling service
+# ---------------------------
+
+echo "Writing to .env to re-enable test service..."
+echo "$test_service_enable_var_name""=1" >> ../.env
+echo -e "OK\n"
+
+echo "Testing that service was re-enabled..."
+cd ..
+. env.sh
+cd tests
+if [ -z "${ENV_SH_TEST-}" ] ; then
+  echo "Error: ENV_SH_TEST env variable is empty."; exit 1
+fi
+if [ "$ENV_SH_TEST" != "$test_service_name" ]; then
+  echo "Error: ENV_SH_TEST env variable is not expected value: expecting: ""$test_service_name""; got: ""$ENV_SH_TEST"; exit 1
+fi
+echo -e "OK\n"
+
+unset ENV_SH_TEST
+if [ -n "${ENV_SH_TEST-}" ]; then
+  echo "Test script error: ENV_SH_TEST variable still set after unsetting."; exit 1
+fi
+
+# Remove service envable var from .env and check
+# ----------------------------------------------
+
+echo "Removing ""$test_service_enable_var_name"" variable from .env..."
+sed -i "/^""$test_service_enable_var_name""/d" ../.env
+if [ "$(grep ^$test_service_enable_var_name ../.env)" ]; then
+  echo "Test script error: ""$test_service_enable_var_name"" variable still set in .env after removing."; exit 1
+fi
+echo -e "OK\n"
+
+echo "Testing that after removing ""$test_service_enable_var_name"" from .env, service does not get enabled..."
+cd ..
+. env.sh
+cd tests
+if [ -n "${ENV_SH_TEST-}" ] ; then
+  echo "Error: ENV_SH_TEST env variable is set despite service being disabled."; exit 1
+fi
+echo -e "OK\n"
+
+# Remove current test service and create new one
+# ----------------------------------------------
+
+echo "Removing test service..."
+rm -rf "$test_service_dir"
+if [ -e "$test_service_dir" ]; then
+  echo "Test script error: failed to remove test_script_dir: ""$test_service_dir"; exit 1
+fi
+
+echo "Creating new service for running tests with random name..."
+rand="$RANDOM"
+if [ -z "$rand" ]; then
+  echo "Error: unable to generate random number: output of \$RANDOM is empty."; exit 1
+fi
+test_service_name="test_""$rand"
+test_service_dir="../services/""$test_service_name"
+if [ -x "$test_service_dir" ]; then
+  echo "Error: test service already exists at ""$test_service_dir"; exit 1
+fi
+echo "mkdir ""$test_service_dir"
+mkdir -p "$test_service_dir"
+echo -e "OK\n"
+
+# Test docker-compose.yml auto load if exists
+# -------------------------------------------
+
+echo "Writing to defaults.env.sh to enable test service by default..."
+defaults_env_sh_path="$test_service_dir""/defaults.env.sh"
+test_service_enable_var_name="SERVICE_ENABLED_${test_service_name^^}"
+echo "export ""$test_service_enable_var_name""=1" > "$defaults_env_sh_path"
+if [ ! -e "$defaults_env_sh_path" ]; then
+  echo "Test script error: failed to write to ""$defaults_env_sh_path"" file does not exist."; exit 1
+fi
+echo "Wrote to ""$defaults_env_sh_path:"
+cat "$defaults_env_sh_path"
+echo -e "OK\n"
+
+echo "Copying docker-compose.test-dynamic-service-loading.yml as docker-compose.yml"
+service_compose_file="$test_service_dir""/docker-compose.yml"
+cp "./docker-compose.test-dynamic-service-loading.yml" "$service_compose_file"
+if [ ! -e "$service_compose_file" ]; then
+  echo "Test script error: docker-compose.yml does not exist in ""$test_service_dir"
+  echo "ls -la ""$test_service_dir"":"
+  ls -la "$test_service_dir"
+  exit 1
+fi
+echo -e "OK\n"
+
+echo "Testing that COMPOSE_FILE env variable contains path to service's docker-compose.yml..."
+cd ..
+. env.sh
+cd tests
+compose_file_root_rel_path="services/""$test_service_name""/docker-compose.yml"
+if [ ! "$(echo $COMPOSE_FILE | grep $compose_file_root_rel_path)" ]; then
+  echo "Error: COMPOSE_FILE environment variable does not contain path to service's compose file: expecting to contain: ""$compose_file_root_rel_path""; contains: ""$COMPOSE_FILE"; exit 1
+fi
+echo -e "OK\n"
+
+echo "Testing docker compose up..."
+cd ..
+docker compose up -d
+if [ ! "$(docker compose ps | grep test-dynamic-service-loading-nginx)" ]; then
+  echo "Error: when running 'docker compose ps' expected to find running container named 'test-dynamic-service-loading-nginx', could not find container:"
+  docker compose ps
+  docker compose down
+  cd tests
+  exit 1
+fi
+docker compose down
+cd tests
 echo -e "OK\n"
